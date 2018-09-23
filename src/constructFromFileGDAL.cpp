f01bd4687ceb3a8549f40991e945fc67232e10aa
rhijmans <r.hijmans@gmail.com>
2018-09-23 00:56:59 -0700

    better extent from gdal

--- src/constructFromFileGDAL.cpp
+++ src/constructFromFileGDAL.cpp
@@ -3,10 +3,17 @@ using namespace std;
 #include <vector>
 #include "spat.h"
 
-
 #include "gdal_priv.h"
 #include "cpl_conv.h" // for CPLMalloc()
 #include "ogr_spatialref.h"
+
+double roundn(double x, int n){
+    int d = 0;
+    if((x * pow(10, n + 1)) - (floor(x * pow(10, n))) > 4) d = 1;
+    x = (floor(x * pow(10, n)) + d) / pow(10, n);
+    return x;
+}
+
 
 bool SpatRaster::constructFromFileGDAL(std::string fname) {
 
@@ -24,13 +31,17 @@ bool SpatRaster::constructFromFileGDAL(std::string fname) {
 	nrow = poDataset->GetRasterYSize();
 	unsigned nlyrs = poDataset->GetRasterCount();
 	source.nlayers = { nlyrs };	
-
+	
 	double adfGeoTransform[6];
 	if( poDataset->GetGeoTransform( adfGeoTransform ) == CE_None ) {
 		double xmin = adfGeoTransform[0]; /* top left x */
+		xmin = roundn(xmin, 9);
 		double xmax = xmin + adfGeoTransform[1] * ncol; /* w-e pixel resolution */
+		xmax = roundn(xmax, 9);
 		double ymax = adfGeoTransform[3]; /* top left y */
+		ymax = roundn(ymax, 9);
 		double ymin = ymax + nrow * adfGeoTransform[5]; /* n-s pixel resolution (negative value) */
+		ymin = roundn(ymin, 9);
 		SpatExtent e(xmin, xmax, ymin, ymax);
 		setExtent(e, false);
 	}
