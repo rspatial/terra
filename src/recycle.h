fe0b2ad22f2ac3c4ee25515c373f542ef16daf19
rhijmans <r.hijmans@gmail.com>
2019-01-25 15:45:51 -0800

    size_t unknown error fix

--- src/recycle.h
+++ src/recycle.h
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with spat. If not, see <http://www.gnu.org/licenses/>.
 
+#include <stddef.h>
 
 template <typename T>
 void recycle(std::vector<T> &v, unsigned n) {
