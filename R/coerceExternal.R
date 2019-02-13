f717d3821145f0a21b04e14395b2c7656fa8fe0c
rhijmans <r,hijmans@gmail.com>
2019-02-13 13:48:19 -0800

    better coercion

--- R/coerceExternal.R
+++ R/coerceExternal.R
@@ -9,11 +9,15 @@
 # 
 # check z values, other attributes such as NAvalue that may have been
 # changed after creation of object from file
+# RAT tables
 
 .fromRasterLayerBrick <- function(from) {
 	f <- filename(from)
 	if (f != "") {
 		r <- rast(f)
+		if (from@file@NAchanged) {
+			warning("changed NA value ignored")
+		}
 		return(r)
 	} else {
 		r <- rast(	nrows=nrow(from), 
@@ -21,27 +25,38 @@
 					nlyrs=nlayers(from),
 					crs=crs(from),
 					extent=extent(from))
-		values(r) <- values(from)			
+		if (hasValues(from)) {
+			values(r) <- values(from)			
+		}	
 		names(r)  <- names(from)
 	}
 	return(r)
 }
+
+.fromRasterStack <- function(from) {
+	x <- from[[1]]
+	n <- nbands(x)
+	if ((n > 1) & (n == nlayers(from))) {
+		ff <- lapply(1:nlayers(from), function(i) { filename(from[[i]]) })
+		if (length(unique(ff)) == 1) {
+			r <- rast(filename(x))
+			return(r)
+		}	
+	} 
+	s <- lapply(1:nlayers(from), function(i) {
+		x <- from[[i]]
+		.fromRasterLayerBrick(x)[[bandnr(x)]]
+	})
+	do.call(c, s)
+}
+
 
 setAs("Raster", "SpatRaster", 
 	function(from) {
 		if (inherits(from, "RasterLayer") | inherits(from, "RasterBrick")) { 
 			.fromRasterLayerBrick(from)			
 		} else {
-			if (raster::canProcessInMemory(from)) {
-				# here we could first check if all bands are used
-				# in the right order, to create a more efficient object
-				# but for now:
-				s <- lapply(1:nlayers(from), function(i) {
-					x <- from[[i]]
-					.fromRasterLayerBrick(x)[[bandnr(x)]]
-				})
-				do.call(c, s)
-			}
+			.fromRasterStack(from)
 		}
 	}
 )
