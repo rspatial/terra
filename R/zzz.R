3e9e9bffc061ff1259db74a4450470a14847c1bf
Robert+1-at-925791393128 <r.hijmans@gmail.com>
2019-05-29 20:18:25 -0700

    z

--- R/zzz.R
+++ R/zzz.R
@@ -3,9 +3,9 @@ loadModule("spat", TRUE)
 
 
 .onAttach <- function(libname, pkgname) {
-	min_raster <- "2.8.17"
+	min_raster <- "2.9.7"
 	tv <- utils::packageVersion("terra")
-	m <- paste0("\nterra ", tv, "\nThis is an early version of the \"terra\" package, for evaluation only\n")
+	m <- paste0("terra ", tv, "\nThis is an early version of the \"terra\" package, for evaluation only\n")
 	e <- try(vv <- utils::packageVersion("raster"), silent=TRUE)
 	if (class(e)[1] == "try-error") {
 		m <- paste0(m,"For this package to work you need to install version ", min_raster, 
