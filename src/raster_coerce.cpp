c948976915939887be0d7ab4abb3599ebbe62ef5
Robert+1-at-925791393128 <r.hijmans@gmail.com>
2019-08-22 18:41:15 -0700

    ren

---
+++ src/raster_coerce.cpp
@@ -0,0 +1,375 @@
+// Copyright (c) 2018-2019  Robert J. Hijmans
+//
+// This file is part of the "spat" library.
+//
+// spat is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// spat is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with spat. If not, see <http://www.gnu.org/licenses/>.
+
+// Based on  public-domain code by Darel Rex Finley, 2007
+// http://alienryderflex.com/polygon_fill/
+
+#include <vector>
+#include "spatRaster.h"
+
+
+std::vector<double> rasterize_polygon(std::vector<double> r, double value, const std::vector<double> &pX, const std::vector<double> &pY, const unsigned nrows, const unsigned ncols, const double xmin, const double ymax, const double rx, const double ry) {
+
+	unsigned n = pX.size();
+	std::vector<unsigned> nCol(n);
+	for (size_t row=0; row<nrows; row++) {
+		double y = ymax - (row+0.5) * ry;
+
+		// find nodes.
+		unsigned nodes = 0;
+		size_t j = n-1;
+		for (size_t i=0; i<n; i++) {
+			if (((pY[i] < y) && (pY[j] >= y)) || ((pY[j] < y) && (pY[i] >= y))) {
+			//	nCol[nodes++]=(int)  (((pX[i] - xmin + (y-pY[i])/(pY[j]-pY[i]) * (pX[j]-pX[i])) + 0.5 * rx ) / rx);
+				double nds = ((pX[i] - xmin + (y-pY[i])/(pY[j]-pY[i]) * (pX[j]-pX[i])) + 0.5 * rx ) / rx;
+				nds = nds < 0 ? 0 : nds;
+		        nds = nds > ncols ? ncols : nds;
+				nCol[nodes] = (unsigned) nds;
+				nodes++;
+			}
+			j = i;
+		}
+
+		std::sort(nCol.begin(), nCol.begin()+nodes);
+		unsigned ncell = ncols * row;
+
+		//  Fill the cells between node pairs.
+		for (size_t i=0; i < nodes; i+=2) {
+			if (nCol[i+1] > 0 && nCol[i] < ncols) {
+				for (size_t col = nCol[i]; col < nCol[i+1]; col++) {
+					r[col + ncell] = value;
+				}
+			}
+		}
+	}
+	return(r);
+}
+
+
+
+
+SpatRaster rasterizePolygons(SpatVector p, SpatRaster r, double background, SpatOptions &opt) {
+
+	SpatRaster out = r.geometry(1);
+
+  	if (!out.writeStart(opt)) { return out; }
+	double value = 0;
+	double resx = out.xres();
+	double resy = out.yres();
+	SpatGeom poly;
+	SpatPart part;
+	SpatHole hole;
+	unsigned n = p.size();
+	unsigned nc = out.ncol();
+	SpatExtent extent = out.getExtent();
+	
+	for (size_t i = 0; i < out.bs.n; i++) {
+		std::vector<double> v(out.bs.nrows[i] * nc, background);
+
+		for (size_t j = 0; j < n; j++) {
+			poly = p.getGeom(j);
+			//value = p.getAtt(j);
+			//if (std::isnan(value)) { value = j;}
+			value = j+1;
+			unsigned np = poly.size();
+
+			for (size_t k = 0; k < np; k++) {
+				part = poly.getPart(k);
+				if (part.hasHoles()) {
+					std::vector<double> vv = rasterize_polygon(v, value, part.x, part.y, out.nrow(), out.ncol(), extent.xmin, extent.ymax, resx, resy);
+					for (size_t h=0; h < part.nHoles(); h++) {
+						hole = part.getHole(h);
+						vv = rasterize_polygon(vv, background, hole.x, hole.y, out.nrow(), out.ncol(), extent.xmin, extent.ymax, resx, resy);
+					}
+					for (size_t q=0; q < vv.size(); q++) {
+						if ((vv[q] != background) && (!std::isnan(vv[q]))) {
+							v[q] = vv[q];
+						}
+					}
+				} else {
+					v = rasterize_polygon(v, value, part.x, part.y, out.nrow(), out.ncol(), extent.xmin, extent.ymax, resx, resy);
+				}
+			}
+		}
+		if (!out.writeValues(v, out.bs.row[i], out.bs.nrows[i], 0, out.ncol())) return out;
+		
+	}
+	out.writeStop();
+	return(out);
+}
+
+
+
+
+std::vector<double> rasterize_line(std::vector<double> r, double value, const std::vector<double> &pX, const std::vector<double> &pY, const unsigned nrows, const unsigned ncols, const double xmin, const double ymax, const double rx, const double ry) {
+	unsigned n = pX.size();
+	for (size_t row=0; row<nrows; row++) {
+		double y = ymax - (row+0.5) * ry;
+		unsigned ncell = ncols * row;
+		for (size_t i=1; i<n; i++) {
+            size_t j = i-1;
+			if (((pY[i] < y) && (pY[j] >= y)) || ((pY[j] < y) && (pY[i] >= y))) {
+				double col = ((pX[i] - xmin + (y-pY[i])/(pY[j]-pY[i]) * (pX[j]-pX[i])) + 0.5 * rx ) / rx;
+				if ((col >= 0) & (col < ncols)) {
+                    r[ncell + col] = value;
+				}
+			}
+		}
+	}
+	return(r);
+}
+
+
+
+SpatRaster rasterizeLines(SpatVector p, SpatRaster r, double background, SpatOptions &opt) {
+
+	SpatRaster out = r.geometry(1);
+  	if (!out.writeStart(opt)) { return out; }
+	double value = 0;
+	double resx = out.xres();
+	double resy = out.yres();
+	SpatGeom line;
+	SpatPart part;
+	unsigned n = p.size();
+	SpatExtent extent = out.getExtent();
+
+	for (size_t i = 0; i < out.bs.n; i++) {
+		std::vector<double> v(out.bs.nrows[i] * out.ncol(), background);
+		for (size_t j = 0; j < n; j++) {
+			line = p.getGeom(j);
+			//value = p.getAtt(j);
+			//if (std::isnan(value)) { value = j;}
+			value = j+1;
+			unsigned nln = line.size();
+			for (size_t k = 0; k < nln; k++) {
+				part = line.getPart(k);
+				v = rasterize_line(v, value, part.x, part.y, out.nrow(), out.ncol(), extent.xmin, extent.ymax, resx, resy);
+			}
+		}
+		if (!out.writeValues(v, out.bs.row[i], out.bs.nrows[i], 0, out.ncol())) return out;
+		
+	}
+	out.writeStop();
+	return(out);
+}
+
+
+SpatRaster rasterizePoints(SpatVector p, SpatRaster r, double background, SpatOptions &opt) {
+	r.setError("not implented yet");
+	return(r);
+}
+
+
+
+SpatRaster SpatRaster::rasterize(SpatVector p, double background, SpatOptions &opt) {
+	std::string gtype = p.type();
+	SpatRaster out = geometry(1);
+	if (gtype == "polygons") {
+		out = rasterizePolygons(p, out, background, opt);
+	} else if (gtype == "lines") {
+		out = rasterizeLines(p, out, background, opt);		
+	}  else {
+		out = rasterizePoints(p, out, background, opt);		
+	}
+	return out;
+}
+
+
+
+
+
+
+SpatVector SpatRaster::as_points(bool values, bool narm) {
+
+// for now assuming one layer
+
+	BlockSize bs = getBlockSize(4);
+	std::vector<double> v, vout;
+	vout.reserve(v.size());
+	SpatVector pv;
+	SpatGeom g;
+	g.gtype = points;
+
+
+    std::vector<std::vector<double>> xy;
+	if ((!values) && (!narm)) {
+        double nc = ncell();
+        for (size_t i=0; i<nc; i++) {
+            xy = xyFromCell(i);
+			SpatPart p(xy[0], xy[1]);
+			g.addPart(p);
+			pv.addGeom(g);
+			g.parts.resize(0);
+        }
+		return pv;
+	}
+
+	if (values) {
+        std::vector<std::string> nms = getNames();
+        for (size_t i=0; i<nlyr(); i++) {
+            pv.lyr.df.add_column(0, nms[i]);
+        }
+	}
+	readStart();
+	unsigned nc = ncol();
+	unsigned nl = nlyr();
+	for (size_t i = 0; i < bs.n; i++) {
+		v = readValues(bs.row[i], bs.nrows[i], 0, nc);
+        unsigned off1 = (bs.row[i] * nc);
+ 		unsigned vnc = bs.nrows[i] * nc;
+		if (narm) {
+            bool foundna = false;
+			for (size_t j=0; j<vnc; j++) {
+				for (size_t lyr=0; lyr<nl; lyr++) {
+                    unsigned off2 = lyr*nc;
+                    foundna = false;
+                    if (std::isnan(v[off2+j])) {
+                        foundna = true;
+                        continue;
+                    }
+                }
+                if (foundna) continue;
+                xy = xyFromCell(off1+j);
+                SpatPart p(xy[0], xy[1]);
+                g.addPart(p);
+                pv.addGeom(g);
+                g.parts.resize(0);
+                if (values) {
+                    for (size_t lyr=0; lyr<nl; lyr++) {
+                        unsigned off2 = lyr*nc;
+                        pv.lyr.df.dv[lyr].push_back(v[off2+j]);
+                    }
+                }
+			}
+		} else { // if (values) {
+			for (size_t j=0; j<vnc; j++) {
+                xy = xyFromCell(off1+j);
+                SpatPart p(xy[0], xy[1]);
+                g.addPart(p);
+                pv.addGeom(g);
+                g.parts.resize(0);
+                for (size_t lyr=0; lyr<nl; lyr++) {
+                    unsigned off2 = lyr*nc;
+                    pv.lyr.df.dv[lyr].push_back(v[off2+j]);
+                }
+			}
+		}
+	}
+	readStop();
+	return(pv);
+}
+
+
+
+
+
+void getCorners(std::vector<double> &x,  std::vector<double> &y, const double &X, const double &Y, const double &xr, const double &yr) {
+	x[0] = X - xr;
+	y[0] = Y - yr;
+	x[1] = X - xr;
+	y[1] = Y + yr;
+	x[2] = X + xr;
+	y[2] = Y + yr;
+	x[3] = X + xr;
+	y[3] = Y - yr;
+	x[4] = x[0];
+	y[4] = y[0];
+}
+
+
+SpatVector SpatRaster::as_polygons(bool values, bool narm) {
+	if (!values) narm=false;
+	SpatVector v;
+	SpatGeom g;
+	g.gtype = polygons;
+	double xr = xres()/2;
+	double yr = yres()/2;
+	std::vector<double> x(5);
+	std::vector<double> y(5);
+	if (!values) {
+		std::vector<double> cells(ncell()) ;
+		std::iota (std::begin(cells), std::end(cells), 0);
+		std::vector< std::vector<double> > xy = xyFromCell(cells);
+		for (size_t i=0; i<ncell(); i++) {
+			getCorners(x, y, xy[0][i], xy[1][i], xr, yr);
+			SpatPart p(x, y);
+			g.addPart(p);
+			v.addGeom(g);
+			g.parts.resize(0);
+		}
+	} else {
+		SpatRaster out = geometry();
+		unsigned nl = nlyr();
+		std::vector<std::vector<double> > att(ncell(), std::vector<double> (nl));
+
+		BlockSize bs = getBlockSize(4);
+		std::vector< std::vector<double> > xy;
+		std::vector<double> atts(nl);
+		for (size_t i=0; i<out.bs.n; i++) {
+			std::vector<double> vals = readBlock(out.bs, i);
+			unsigned nc=out.bs.nrows[i] * ncol();
+			for (size_t j=0; j<nc; j++) {
+				for (size_t k=0; k<nl; k++) {
+					size_t kk = j + k * nl;
+					att[nc+j][k] = vals[kk];
+				}
+				xy = xyFromCell(nc+j);
+				getCorners(x, y, xy[0][0], xy[1][0], xr, yr);
+				SpatPart p(x, y);
+				g.addPart(p);
+				v.addGeom(g);
+				g.parts.resize(0);
+
+			}
+		}
+		SpatDataFrame df;
+		std::vector<std::string> nms = getNames();
+		for (size_t i=0; i<att.size(); i++) {
+			df.add_column(att[i], nms[i]);
+		}
+	}
+	v.setCRS(getCRS());
+	return(v);
+}
+
+
+
+
+SpatVector SpatVector::as_lines() {
+	SpatVector v = *this;
+	if (lyr.geoms[0].gtype != polygons) {
+		v.setError("this only works for polygons");
+		return v;
+	}
+	for (size_t i=0; i<size(); i++) {
+		for (size_t j=0; j < v.lyr.geoms[i].size(); j++) {
+			SpatPart p = v.lyr.geoms[i].parts[j];
+			if (p.hasHoles()) {
+				for (size_t k=0; k < p.nHoles(); k++) {
+					SpatHole h = p.getHole(k);
+					SpatPart pp(h.x, h.y);
+					v.lyr.geoms[i].addPart(pp);
+				}
+				p.holes.resize(0);
+				v.lyr.geoms[i].parts[j] = p;
+			}
+		}
+		v.lyr.geoms[i].gtype = lines;
+	}
+	return(v);
+}
+
---
+++ src/raster_stats.cpp
@@ -0,0 +1,416 @@
+// Copyright (c) 2018-2019  Robert J. Hijmans
+//
+// This file is part of the "spat" library.
+//
+// spat is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// spat is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with spat. If not, see <http://www.gnu.org/licenses/>.
+
+#include "spatRaster.h"
+#include <limits>
+#include <set>
+#include <cmath>
+#include <algorithm>
+
+#include "vecmath.h"
+#include "math_utils.h"
+#include "string_utils.h"
+
+std::map<double, unsigned> table(std::vector<double> &v) {
+	std::map<double, unsigned> count;
+	for_each( v.begin(), v.end(), [&count]( double val ){ 
+			if(!std::isnan(val)) count[val]++; 
+		} 
+	);	
+	return count;
+}
+
+
+std::map<double, unsigned> ctable(std::map<double, unsigned> &x, std::map<double, unsigned> &y) {
+	for(auto p : y) {
+		x[p.first] += p.second;
+	}	
+	return(x);
+}
+
+
+std::vector<double> vtable(std::map<double, unsigned> &x) {
+	std::vector<std::vector<double>> out(2);
+	for( auto p : x ) {
+		out[0].push_back(p.first);
+		out[1].push_back(p.second);
+	}  
+	out[0].insert(out[0].end(), out[1].begin(), out[1].end());
+	return out[0];
+}
+
+
+
+std::vector<std::vector<double>> SpatRaster::freq(bool bylayer) {
+	std::vector<std::vector<double>> out;
+	if (!hasValues()) return out;
+	BlockSize bs = getBlockSize(4);
+	unsigned nc = ncol();
+	unsigned nl = nlyr();
+	readStart();
+	if (bylayer) {
+		out.resize(nl);
+		std::vector<std::map<double, unsigned>> tabs(nl);
+		for (size_t i = 0; i < bs.n; i++) {
+			unsigned n = bs.nrows[i] * nc;
+			std::vector<double> v = readValues(bs.row[i], bs.nrows[i], 0, nc);
+			for (size_t lyr=0; lyr<nl; lyr++) {
+				unsigned off = lyr*n;
+				std::vector<double> vv(v.begin()+off, v.begin()+off+n);
+				std::map<double, unsigned> tab = table(vv);
+				tabs[lyr] = ctable(tabs[lyr], tab);
+			}
+		}
+		for (size_t lyr=0; lyr<nl; lyr++) {
+			out[lyr] = vtable(tabs[lyr]);
+		}
+	} else {
+		out.resize(1);
+		std::map<double, unsigned> tabs;
+		for (size_t i = 0; i < bs.n; i++) {
+			std::vector<double> v = readValues(bs.row[i], bs.nrows[i], 0, nc);
+			std::map<double, unsigned> tab = table(v);
+			tabs = ctable(tabs, tab);
+		}
+		out[0] = vtable(tabs);
+	}
+	readStop();
+	return(out);
+}
+
+
+static inline double interpolate(double x, double y1, double y2, unsigned x1, unsigned x2) {
+	double denom = (x2-x1);
+	return y1 + (x-x1) * (y2-y1)/denom;
+}
+
+
+static inline std::vector<double> vquantile(std::vector<double> v, const std::vector<double>& probs, bool narm) {
+	size_t n = v.size();
+    if (n==0) {
+        return std::vector<double>(probs.size(), NAN);
+    }
+    if (n == 1) {
+        return std::vector<double>(probs.size(), v[0]);
+    }
+	na_omit(v);
+	if ((!narm) & (v.size() < n)) {
+        return std::vector<double>(probs.size(), NAN);
+	}
+	n = v.size();
+    std::sort(v.begin(), v.end());
+
+	size_t pn = probs.size();
+	std::vector<double> q(pn);
+		
+    for (size_t i = 0; i < pn; ++i) {
+		double x = probs[i] * (n-1);
+		unsigned x1 = floor(x);
+		unsigned x2 = ceil(x);
+		if (x1 == x2) {
+			q[i] = v[x1];
+		} else {
+			q[i] = interpolate(x, v[x1], v[x2], x1, x2);
+		}	
+    }
+    return q;
+}
+
+
+SpatRaster SpatRaster::quantile(std::vector<double> probs, bool narm, SpatOptions &opt) {
+	size_t n = probs.size();
+	if (n == 0) {
+		SpatRaster out = geometry(1);
+		out.setError("no probs");
+		return out;
+	}
+	double pmin = vmin(probs, false);
+	double pmax = vmin(probs, false);
+	if ((std::isnan(pmin)) | (std::isnan(pmax)) | (pmin < 0) | (pmax > 1)) {
+		SpatRaster out = geometry(1);
+		out.setError("intvalid probs");
+		return out;		
+	}	
+	SpatRaster out = geometry(probs.size());
+	out.source[0].names = double_to_string(probs, "q");
+  	if (!hasValues()) { return out; }
+
+  	if (!out.writeStart(opt)) { return out; }
+	readStart();
+	unsigned nl = nlyr();
+	std::vector<double> v(nl);
+
+	for (size_t i = 0; i < out.bs.n; i++) {
+		std::vector<double> a = readBlock(out.bs, i);
+		unsigned nc = out.bs.nrows[i] * out.ncol();
+		std::vector<double> b(nc * n);
+		for (size_t j=0; j<nc; j++) {
+			for (size_t k=0; k<nl; k++) {
+				v[k] = a[j+k*nc];
+			}
+			std::vector<double> p = vquantile(v, probs, narm);
+			for (size_t k=0; k<n; k++) {
+				b[j+(k*nc)] = p[k];
+			}
+		}
+		if (!out.writeValues(b, out.bs.row[i], out.bs.nrows[i], 0, ncol())) return out;
+	}
+	out.writeStop();
+	readStop();
+	return(out);
+}
+
+
+
+
+
+void unique_values_alt(std::vector<double> &d) {
+	d.erase(std::remove_if(d.begin(), d.end(),
+            [](const double& value) { return std::isnan(value); }), d.end());
+	std::sort(d.begin(), d.end());
+	d.erase(std::unique(d.begin(), d.end()), d.end());
+}
+
+
+void unique_values(std::vector<double> &d) {
+	d.erase(std::remove_if(d.begin(), d.end(),
+            [](const double& value) { return std::isnan(value); }), d.end());
+	std::set<double> u { d.begin(), d.end()};
+	std::copy(u.begin(), u.end(), d.begin());
+	d.erase(d.begin()+u.size(), d.end());
+}
+
+
+std::vector<std::vector<double>> SpatRaster::unique(bool bylayer) {
+
+	std::vector<std::vector<double>> out;
+	if (!hasValues()) return out;
+
+	constexpr double lowest_double = std::numeric_limits<double>::lowest();
+
+	BlockSize bs = getBlockSize(4);
+	unsigned nc = ncol();
+	unsigned nl = nlyr();
+	readStart();
+	out.resize(nl);
+
+	if (nl == 1) bylayer = true;
+	if (bylayer) {
+		for (size_t i = 0; i < bs.n; i++) {
+			unsigned n = bs.nrows[i] * nc;
+			std::vector<double> v = readValues(bs.row[i], bs.nrows[i], 0, nc);
+			for (size_t lyr=0; lyr<nl; lyr++) {
+				unsigned off = lyr*n;
+				out[lyr].insert(out[lyr].end(), v.begin()+off, v.begin()+off+n);
+				unique_values(out[lyr]);
+			}
+		}
+	} else {
+		std::vector<std::vector<double>> temp;
+		for (size_t i = 0; i < bs.n; i++) {
+			unsigned n = bs.nrows[i] * nc;
+			std::vector<std::vector<double>> m(n, std::vector<double>(nl));
+			std::vector<double> v = readValues(bs.row[i], bs.nrows[i], 0, nc);
+			for (size_t j = 0; j < v.size(); j++) {
+				if (std::isnan(v[j])) v[j] = lowest_double;
+			}
+
+			for (size_t lyr=0; lyr<nl; lyr++) {
+				unsigned off = lyr*n;
+				for (size_t j=0; j<n; j++) {
+					m[j][lyr] = v[off+j];
+				}
+			}
+			std::sort(m.begin(), m.end());
+			m.erase(std::unique(m.begin(), m.end()), m.end());
+			for (size_t j=0; j<m.size(); j++) {
+				temp.insert(temp.end(), m[j]);
+			}
+		}
+		std::sort(temp.begin(), temp.end());
+		temp.erase(std::unique(temp.begin(), temp.end()), temp.end());
+		for (size_t i = 0; i < temp.size(); i++) {
+			for (size_t j = 0; j < temp[0].size(); j++) {
+				out[j].resize(temp.size());
+				if (temp[i][j] == lowest_double) {
+					out[j][i] = NAN;
+				} else {
+					out[j][i] = temp[i][j];
+				}
+			}
+		}
+	}
+	readStop();
+
+	return(out);
+}
+
+
+
+
+void jointstats(const std::vector<double> &u, const std::vector<double> &v, const std::vector<double> &z, std::string fun, bool narm, std::vector<double>& out, std::vector<double> &cnt) {
+
+	std::vector<double> cmp;
+	std::vector<bool> done(z.size(), false);
+	//recycle(v, z);
+
+	for (size_t j=0; j<u.size(); j++) {
+		cmp.resize(0);
+		for (size_t k=0; k<v.size(); k++) {
+			if (j==0) {
+				if (std::isnan(z[k])) {
+					done[k] = true;
+				}
+			}
+			if (!done[k]) {
+				if (z[k] == u[j]) {
+					if (!(narm & std::isnan(v[k]))) {
+						cmp.push_back(v[k]);
+						done[k]=true;
+					}
+				}
+			}
+		}
+
+		if (fun=="sum") {
+			double s = vsum(cmp, narm);
+			out[j] = s + out[j];
+		} else if (fun=="mean") {
+			double s = vsum(cmp, narm);
+			if (narm) {
+				for (size_t k=1; k<cmp.size(); k++) {
+					cnt[j] += !std::isnan(cmp[k]);
+				}
+			} else {
+				cnt[j] += cmp.size();
+			}
+			out[j] = s + out[j];
+		} else if (fun == "min") {
+			double m = vmin(cmp, narm);
+			if (narm) {
+				if (!std::isnan(m)) {
+					if (cnt[j] == 0) {
+						out[j] = m;
+						cnt[j] = 1;
+					} else {
+						out[j] = std::min(m, out[j]);
+					}
+				}
+			} else {
+				if (cnt[j] == 0) {
+					out[j] = m;
+					cnt[j] = 1;
+				} else {
+					out[j] = std::min(m, out[j]);
+				}
+			}
+		} else if (fun == "max") {
+			double m = vmax(cmp, narm);
+			if (narm) {
+				if (!std::isnan(m)) {
+					if (cnt[j] == 0) {
+						out[j] = m;
+						cnt[j] = 1;
+					} else {
+						out[j] = std::max(m, out[j]);
+					}
+				}
+			} else {
+				if (cnt[j] == 0) {
+					out[j] = m;
+					cnt[j] = 1;
+				} else {
+					out[j] = std::max(m, out[j]);
+				}
+			}
+		}
+	}
+}
+
+
+
+
+SpatDataFrame SpatRaster::zonal(SpatRaster z, std::string fun, bool narm) {
+
+	SpatDataFrame out;
+	std::vector<std::string> f {"sum", "mean", "min", "max"};
+	if (std::find(f.begin(), f.end(), fun) == f.end()) {
+		out.setError("not a valid function");
+		return(out);
+	}
+	if (!hasValues()) {
+		out.setError("SpatRaster has no values");
+		return(out);
+	}
+	if (!z.hasValues()) {
+		out.setError("zonal SpatRaster has no values");
+		return(out);
+	}
+	if (!compare_geom(z, false, true)) {
+		out.setError("dimensions and/or extent do not match");
+		return(out);
+	}
+
+	if (z.nlyr() > 1) {
+		SpatOptions opt;
+		std::vector<unsigned> lyr = {0};
+		z = z.subset(lyr, opt);
+	}
+
+	std::vector<std::vector<double>> uq = z.unique(true);
+	std::vector<double> u = uq[0];
+	std::vector<std::vector<double>> stats(nlyr(), std::vector<double>(u.size()));
+	std::vector<std::vector<double>> cnt(nlyr(), std::vector<double>(u.size()));
+
+	readStart();
+	z.readStart();
+	BlockSize bs = getBlockSize(8);
+	for (size_t i=0; i<bs.n; i++) {
+		std::vector<double> v =   readValues(bs.row[i], bs.nrows[i], 0, ncol());
+		std::vector<double> zv = z.readValues(bs.row[i], bs.nrows[i], 0, ncol());
+		unsigned off = bs.nrows[i] * ncol() ;
+		for (size_t lyr=0; lyr<nlyr(); lyr++) {
+			unsigned offset = lyr * off;
+			std::vector<double> vv = {  v.begin()+offset,  v.begin()+offset+off };
+			jointstats(u, vv, zv, fun, narm, stats[lyr], cnt[lyr]);
+		}
+	}
+	readStop();
+	z.readStop();
+
+	if (fun=="mean") {
+		for (size_t lyr=0; lyr<nlyr(); lyr++) {
+			for (size_t j=0; j<u.size(); j++) {
+				if (cnt[lyr][j] > 0) {
+					stats[lyr][j] = stats[lyr][j] / cnt[lyr][j];
+				} else {
+					stats[lyr][j] = NAN;
+				}
+			}
+		}
+	}
+
+	out.add_column(u, "zone");
+	std::vector<std::string> nms = getNames();
+	for (size_t i=0; i<nlyr(); i++) {
+		out.add_column(stats[i], nms[i]);
+	}
+	return(out);
+}
+
+
+
