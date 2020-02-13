0232976f505d073fc0b9252ee833e40225bfc31e
Robert+1-at-925791393128 <r.hijmans@gmail.com>
2020-02-13 09:30:31 -0800

    x

--- src/hdr.cpp
+++ src/hdr.cpp
@@ -5,10 +5,10 @@
 
 std::vector<std::string> hdr_read(std::string filename) {
 	CSimpleIniA ini(true, false, false);
-//	char ss[filename.length()];
-	char *ss = new char[filename.length()];
-	
-	strcpy(ss, filename.c_str());
+////	char ss[filename.length()];
+//	char *ss = new char[filename.length()];
+//  strcpy(ss, filename.c_str());
+	const char *ss = filename.c_str();
 	SI_Error rc = ini.LoadFile(ss);
 	
 	delete[] ss;
