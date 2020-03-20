65fe6eafcf62f65b074eea5df712c3ca57de743a
Michael Sumner <mdsumner@gmail.com>
2020-03-20 23:52:00 +1100

    matrix-input remove stray code, fix crs input, +tests

--- R/rast.R
+++ R/rast.R
@@ -5,14 +5,14 @@
 
 
 
-setMethod("rast", signature(x="missing"), 
+setMethod("rast", signature(x="missing"),
 	function(x, nrows=180, ncols=360, nlyrs=1, xmin=-180, xmax=180, ymin=-90, ymax=90, crs, extent, resolution, ...) {
 
 		if (missing(extent)) {	extent <- ext(xmin, xmax, ymin, ymax) }
 		e <- as.vector(extent)
-	
+
 		if (missing(crs)) {
-			if (e[1] > -360.01 & e[2] < 360.01 & e[3] > -90.01 & e[4] < 90.01) { 
+			if (e[1] > -360.01 & e[2] < 360.01 & e[3] > -90.01 & e[4] < 90.01) {
 				crs <- "+proj=longlat +datum=WGS84"
 			} else {
 				crs <- as.character(NA)
@@ -20,31 +20,31 @@ setMethod("rast", signature(x="missing"),
 		} else {
 			crs <- as.character(crs)
 		}
-		
+
 
 		r <- methods::new("SpatRaster")
 		r@ptr <- SpatRaster$new(c(nrows, ncols, nlyrs), e, crs)
-		
+
 		if (!missing(resolution)) {
 		#	res(r) <- resolution
 			stop()
 		}
-		
-		show_messages(r, "rast")		
+
+		show_messages(r, "rast")
 	}
 )
 
 
-setMethod("rast", signature(x="SpatExtent"), 
+setMethod("rast", signature(x="SpatExtent"),
 	function(x, nrows=10, ncols=10, nlyrs=1, crs="", ...) {
-		e <- as.vector(x)		
+		e <- as.vector(x)
 		r <- methods::new("SpatRaster")
 		r@ptr <- SpatRaster$new(c(nrows, ncols, nlyrs), e, crs)
-		show_messages(r, "rast")		
+		show_messages(r, "rast")
 	}
 )
 
-setMethod("rast", signature(x="SpatVector"), 
+setMethod("rast", signature(x="SpatVector"),
 	function(x, nrows=10, ncols=10, nlyrs=1, ...) {
 		rast(ext(x), nrows=nrows, ncols=ncols, nlyrs=nlyrs, crs=crs(x), ...)
 	}
@@ -63,7 +63,7 @@ setMethod("rast", signature(x="SpatVector"),
 	return(x)
 }
 
-setMethod("rast", signature(x="character"), 
+setMethod("rast", signature(x="character"),
 	function(x, ...) {
 		if (length(x) == 0) {
 			stop("provide valid file name(s)")
@@ -71,12 +71,12 @@ setMethod("rast", signature(x="character"),
 		f <- .fullFilename(x)
 		r <- methods::new("SpatRaster")
 		r@ptr <- SpatRaster$new(f)
-		show_messages(r, "rast")		
+		show_messages(r, "rast")
 	}
 )
 
 
-setMethod("rast", signature(x="SpatRaster"), 
+setMethod("rast", signature(x="SpatRaster"),
 	function(x, nlyrs=nlyr(x), ...) {
 		r <- methods::new("SpatRaster")
 		dims <- dim(x)
@@ -84,38 +84,40 @@ setMethod("rast", signature(x="SpatRaster"),
 		dims[3] <- nlyrs
 		r@ptr <- SpatRaster$new(dims, as.vector(ext(x)), crs(x))
 		# also need the keep the names ?
-		show_messages(r, "rast")		
+		show_messages(r, "rast")
 	}
 )
 
 
 
 
-setMethod("rast", signature(x="array"), 
+setMethod("rast", signature(x="array"),
 	function(x, ...) {
 		dims <- dim(x)
 		if (length(dims) > 3) {
-			stop("cannot handle an array with more than 3 dimensions")		
+			stop("cannot handle an array with more than 3 dimensions")
 		}
 		r <- methods::new("SpatRaster")
 		r@ptr <- SpatRaster$new(dims, c(0, dims[2], 0, dims[1]), "")
 		values(r) <- x
-		show_messages(r, "rast")		
+		show_messages(r, "rast")
 	}
 )
 
 
 
-setMethod("rast", signature(x="Raster"), 
+setMethod("rast", signature(x="Raster"),
 	function(x, ...) {
 		methods::as(x, "SpatRaster")
 	}
 )
 
 
-.rastFromXYZ <- function(xyz, digits=6, ...) {
+.rastFromXYZ <- function(xyz, digits=6, crs = NA, ...) {
 
 	ln <- colnames(xyz)
+	## xyz might not have colnames, or might have "" names
+	if (any(nchar(ln) < 1)) ln <- make.names(ln)
 	if (inherits(xyz, "data.frame")) {
 		xyz <- as.matrix(xyz)
 		xyz <- matrix(as.numeric(xyz), ncol=ncol(xyz), nrow=nrow(xyz))
@@ -134,13 +136,13 @@ setMethod("rast", signature(x="Raster"),
 	if ( q > 0 ) {
 		stop("x cell sizes are not regular")
 	}
-	
+
 	y <- sort(unique(xyz[,2]))
 	dy <- y[-1] - y[-length(y)]
-	# probably a mistake to use the line below 
-	# Gareth Davies suggested that it be removed 
+	# probably a mistake to use the line below
+	# Gareth Davies suggested that it be removed
 	# dy <- round(dy, digits)
-	
+
 	ry <- min(dy)
 	for (i in 1:5) {
 		ry <- ry / i
@@ -152,14 +154,14 @@ setMethod("rast", signature(x="Raster"),
 	if ( q > 0 ) {
 		stop("y cell sizes are not regular")
 	}
-	
+
 	minx <- min(x) - 0.5 * rx
 	maxx <- max(x) + 0.5 * rx
 	miny <- min(y) - 0.5 * ry
 	maxy <- max(y) + 0.5 * ry
-	
+
 	d <- dim(xyz)
-	r <- rast(xmin=minx, xmax=maxx, ymin=miny, ymax=maxy, crs=crs, nl=d[2]-2)	
+	r <- rast(xmin=minx, xmax=maxx, ymin=miny, ymax=maxy, crs=crs, nl=d[2]-2)
 	res(r) <- c(rx, ry)
 	cells <- cellFromXY(r, xyz[,1:2])
 	if (d[2] > 2) {
@@ -167,24 +169,23 @@ setMethod("rast", signature(x="Raster"),
 		v <- rep(NA, ncell(r))
 		v[cells] <- xyz[,3:d[2]]
 		values(r) <- v
-	} 	
+	}
 	return(r)
-}	
-	
+}
 
 
-setMethod("rast", signature(x="matrix"), 
+
+setMethod("rast", signature(x="matrix"),
 	function(x, crs=NA, type="", ...) {
-		res <- rep_len(res, 2)
 		if (type == "xyz") {
-			r <- .rastFromXYZ(x, ...)
+			r <- .rastFromXYZ(x, crs = crs, ...)
 		} else {
 			r <- methods::new("SpatRaster")
 			r@ptr <- SpatRaster$new(c(dim(x), 1), c(0, ncol(x), 0, nrow(x)), "")
 			values(r) <- t(x)
 		}
 		if (!is.na(crs)) crs(r) <- crs
-		show_messages(r, "rast")		
+		show_messages(r, "rast")
 	}
 )
 
