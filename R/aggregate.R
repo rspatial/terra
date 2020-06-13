49d17b07f76d79d49ce0513ab64598e64b762899
Robert+1-at-925791393128 <r.hijmans@gmail.com>
2020-06-12 19:32:52 -0700

    fixes #44

--- R/aggregate.R
+++ R/aggregate.R
@@ -57,7 +57,7 @@ function(x, fact=2, fun="mean", ..., nodes=1, filename="", overwrite=FALSE, wopt
 		dims <- x@ptr$get_aggregate_dims(fact)
 		b <- x@ptr$getBlockSize(4)		
 		
-		nr <- floor(b$nrows[1] / fact[1]) * fact[1]
+		nr <- max(1, floor(b$nrows[1] / fact[1])) * fact[1]
 		nrs <- rep(nr, floor(nrow(x)/nr))
 		d <- nrow(x) - sum(nrs) 
 		if (d > 0) nrs <- c(nrs, d)
@@ -65,9 +65,9 @@ function(x, fact=2, fun="mean", ..., nodes=1, filename="", overwrite=FALSE, wopt
 		b$nrows <- nrs
 		b$n <- length(nrs)
 		outnr <- ceiling(b$nrows / fact[1])
-		outrows  <- c(0, cumsum(outnr))[1:length(outnr)] + 1
-		
+		outrows  <- c(0, cumsum(outnr))[1:length(outnr)] + 1	
 		nc <- ncol(x)	
+		
 		if (nodes > 1) {
 			doPar <- TRUE
 			cls <- parallel::makeCluster(nodes)
@@ -94,7 +94,7 @@ function(x, fact=2, fun="mean", ..., nodes=1, filename="", overwrite=FALSE, wopt
 			for (i in 1:b$n) {
 				v <- readValues(x, b$row[i], b$nrows[i], 1, nc)
 				v <- x@ptr$get_aggregates(v, b$nrows[i], dims)
-				v <- sapply(v, fun, ...)			
+				v <- sapply(v, fun, ...)	
 				if (length(v) != outnr[i] * prod(dims[5:6])) {
 					stop("this function does not return the correct number of values")
 				}
