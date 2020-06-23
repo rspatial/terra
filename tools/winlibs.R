6087b2d0e03a3acf2c839429f0b9220de7f429ce
Robert+1-at-925791393128 <r.hijmans@gmail.com>
2020-06-22 22:45:04 -0700

    m

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,27 +1,15 @@
-# taken from "sf"
-# For details see: https://github.com/rwinlib/gdal2
-#VERSION <- commandArgs(TRUE)
-#if (!file.exists(sprintf("../windows/gdal2-%s/include/gdal/gdal.h", VERSION))) {
-#  download.file(sprintf("https://github.com/rwinlib/gdal2/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
-#  dir.create("../windows", showWarnings = FALSE)
-#  unzip("lib.zip", exdir = "../windows")
-#  unlink("lib.zip")
-#}
-
 
 if(getRversion() < "3.3.0") {
-  stop("Your version of R is too old. This package requires R-3.3.0 or newer on Windows.")
+	stop("Your version of R is too old. This package requires R-3.3.0 or newer on Windows.")
 }
-
 
 # Download gdal-3.* from rwinlib
 VERSION <- commandArgs(TRUE)
 testfile <- sprintf("../windows/gdal3-%s/include/gdal-%s/gdal.h", VERSION, VERSION)
 if(!file.exists(testfile)){
-  #if(getRversion() < "3.3.0") setInternet2()
-  download.file(sprintf("https://github.com/rwinlib/gdal3/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
-  dir.create("../windows", showWarnings = FALSE)
-  unzip("lib.zip", exdir = "../windows")
-  unlink("lib.zip")
+	download.file(sprintf("https://github.com/rwinlib/gdal3/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
+	dir.create("../windows", showWarnings = FALSE)
+	unzip("lib.zip", exdir = "../windows")
+	unlink("lib.zip")
 }
 
