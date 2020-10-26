03d01a6163f5171a277744d497f077db1b2bc31e
rhijmans <r.hijmans@gmail.com>
2020-10-25 22:00:57 -0700

    restore

--- src/ram.cpp
+++ src/ram.cpp
@@ -1,10 +1,26 @@
-// Robert Hijmans with improvments by Ben Fasoli
-// https://github.com/rspatial/raster/pull/175
+// Copyright (c) 2018-2020  Robert J. Hijmans
+//
+// This file is part of the "spat" library.
+//
+// spat is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// spat is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with spat. If not, see <http://www.gnu.org/licenses/>.
 
-#ifdef _WIN32 
+#ifdef _WIN32
 #include <windows.h>
 #elif __linux__
-#include <stdio.h>
+#include "sys/types.h"
+#include "sys/sysinfo.h"
+#include <unistd.h>
 #elif __APPLE__
 #include <mach/vm_statistics.h>
 #include <mach/mach_types.h>
@@ -12,55 +28,38 @@
 #include <mach/mach_host.h>
 #endif
 
+
 double availableRAM() {
+//https://stackoverflow.com/questions/38490320/how-to-query-amount-of-allocated-memory-on-linux-and-osx
+//https://stackoverflow.com/questions/63166/how-to-determine-cpu-and-memory-consumption-from-inside-a-process
 	double ram = 1e+9;
-	// return available RAM
+	// return available RAM in number of double (8 byte) cells.
 	#ifdef _WIN32
 		MEMORYSTATUSEX statex;
 		statex.dwLength = sizeof(statex);
 		GlobalMemoryStatusEx(&statex);
 		ram = statex.ullAvailPhys;
 	#elif __linux__
-		// source available memory from /proc/meminfo
-		// default to searching for MemAvailable field (kernel versions >= 3.14)
-		FILE *fp = popen("awk '/MemAvailable/ {print $2}' /proc/meminfo", "r");
-		if (fp == NULL) {
-			return ram;
-		}
-		double ramkb;
-		int ok = fscanf(fp, "%lf", &ramkb);  // returned in kB
-		pclose(fp);
-		if (ramkb > 0) {
-			return ramkb * 1000.;
-		}
-		
-		// fallback to estimating memory from other fields if MemAvailable not found
-		FILE *fp2 = popen("awk -v low=$(grep low /proc/zoneinfo | awk '{k+=$2}END{print k}') '{a[$1]=$2}END{print a[\"MemFree:\"]+a[\"Active(file):\"]+a[\"Inactive(file):\"]+a[\"SReclaimable:\"]-(12*low);}' /proc/meminfo", "r");
-		if (fp2 == NULL) {
-			return ram;
-		}
-		ok = fscanf(fp2, "%lf", &ramkb);  // returned in kB
-		pclose(fp2);
-		if (ramkb > 0) {
-			return ramkb * 1000.;
-		}
+		struct sysinfo memInfo;
+		sysinfo (&memInfo);
+		ram = memInfo.freeram;
 	#elif __APPLE__
 		vm_size_t page_size;
 		mach_port_t mach_port;
 		mach_msg_type_number_t count;
 		vm_statistics64_data_t vm_stats;
-
 		mach_port = mach_host_self();
 		count = sizeof(vm_stats) / sizeof(natural_t);
 		if (KERN_SUCCESS == host_page_size(mach_port, &page_size) &&
-			KERN_SUCCESS == host_statistics64(mach_port, HOST_VM_INFO,
+					KERN_SUCCESS == host_statistics64(mach_port, HOST_VM_INFO,
 											(host_info64_t)&vm_stats, &count)) {
-			long long free_memory = ((int64_t)vm_stats.free_count +
-                               (int64_t)vm_stats.inactive_count) * (int64_t)page_size;
-			ram = free_memory;
-		//https://stackoverflow.com/questions/63166/how-to-determine-cpu-and-memory-consumption-from-inside-a-process
+			ram = (int64_t)vm_stats.free_count * (int64_t)page_size;
+			
+			ram = ram * 5; // purely empirical. The estimate seemed much too low. Needs work.
 		}
+		
 	#endif
-	
-	return ram;
+	return ram / 8;  // 8 bytes for each double
 }
+
+
