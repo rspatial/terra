b4501a5726221af4a25dc01cd6bb28870a7af161
Robert+1-at-925791393128 <r.hijmans@gmail.com>
2020-10-31 13:37:00 -0700

    ani

--- R/animate.R
+++ R/animate.R
@@ -1,7 +1,7 @@
 
 
 setMethod("animate", signature(x="SpatRaster"), 
-function(x, pause=0.25, main, zlim, maxcell=50000, n=10, ...) {
+function(x, pause=0.25, main, range, maxcell=50000, n=10, ...) {
 	nl <- nlyr(x)
 	if (missing(main)) {
 		main <- names(x)
@@ -9,15 +9,15 @@ function(x, pause=0.25, main, zlim, maxcell=50000, n=10, ...) {
 
 	x <- spatSample(x, size=maxcell, method="regular", as.raster=TRUE)
 	
-	if (missing(zlim)) {
+	if (missing(range)) {
 		mnmx <- minmax(x)
-		zlim <- c(min(mnmx[1,]), max(mnmx[2,]))
+		range <- c(min(mnmx[1,]), max(mnmx[2,]))
 	}
 	
 	i <- 1
 	reps <- 0
     while (reps < n) {
-        plot(x[[i]], main = main[i], zlim=zlim, maxcell=Inf, ...)
+        plot(x[[i]], main = main[i], range=range, maxcell=Inf, ...)
         grDevices::dev.flush()
         Sys.sleep(pause)
         i <- i + 1
