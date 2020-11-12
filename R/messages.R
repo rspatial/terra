ce01560e47708841dbc410c6d990215770b08140
Robert+1-at-925791393128 <r.hijmans@gmail.com>
2020-11-11 17:10:08 -0800

    m

--- R/messages.R
+++ R/messages.R
@@ -31,23 +31,23 @@ show_messages <- function(x, f="") {
 .mem_info <- function(x, n=1, print=TRUE) {
 	n <- max(0,n)
 	opt <- .getOptions()
-	opt.ncopies = 1;
+	opt$ncopies = n;
 	v <- x@ptr$mem_needs(opt)
 	if (print) {
 		gb <- 1024^3 / 8  # 
-		cat("\n----------------------")
+		cat("\n------------------------")
 		cat("\nMemory (GB) ")
-		cat("\n----------------------")
-		cat(paste("\navailable      :",  round(v[2] / gb, 2)))
-		cat(paste0("\nallowed (", round(100* v[3]) , "%)  : ", round(v[3] * v[2] / gb, 2)))
-		cat(paste("\nneeded (1 copy):", round(v[1] / gb, 2)))
-		cat("\n----------------------")
-		cat(paste("\nproc in memory :", round(v[5]) != 0))
-		cat(paste("\nnr chunks      :", ceiling(nrow(x)/v[4])))
-		cat("\n----------------------\n")
+		cat("\n------------------------")
+		cat(paste("\navailable       :",  round(v[2] / gb, 2)))
+		cat(paste0("\nallowed (", round(100* v[3]) , "%)   : ", round(v[3] * v[2] / gb, 2)))
+		cat(paste0("\nneeded (n=", n, ")    : ", round(v[1] / gb, 2)))
+		cat("\n------------------------")
+		cat(paste("\nproc in memory  :", round(v[5]) != 0))
+		cat(paste("\nnr chunks       :", ceiling(nrow(x)/v[4])))
+		cat("\n------------------------\n")
 	} else {
 		names(v) <- c("needed", "available", "memfrac", "chunksize")
 		v
 	}
 }
-	
+
