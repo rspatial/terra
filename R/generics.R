9216cd78548399d7052523e7bf0ba1a2805e866c
rhijmans <r.hijmans@gmail.com>
2021-05-08 20:38:14 -0700

    ff

--- R/generics.R
+++ R/generics.R
@@ -55,9 +55,9 @@ setMethod("align", signature(x="SpatExtent", y="numeric"),
 )
 
 setMethod("cellSize", signature(x="SpatRaster"), 
-	function(x, mask=TRUE, unit="m", correct=TRUE, filename="", ...) {
+	function(x, mask=TRUE, unit="m", transform=TRUE, filename="", ...) {
 		opt <- spatOptions(filename, ...)
-		x@ptr <- x@ptr$rst_area(mask, unit, correct, opt)
+		x@ptr <- x@ptr$rst_area(mask, unit, transform, opt)
 		messages(x, "cellSize")
 	}
 )
