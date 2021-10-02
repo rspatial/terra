ba8e58adf01ebb9821c9b359c1402bf7ccf87eca
rhijmans <r.hijmans@gmail.com>
2021-10-01 17:08:28 -0700

    x

--- R/levels.R
+++ R/levels.R
@@ -228,6 +228,42 @@ setMethod ("as.numeric", "SpatRaster",
 )
 
 	
+
+catLayer <- function(x, index, ...) {
+		stopifnot(nlyr(x) == 1)
+		if (!is.factor(x)) return(x)
+		g <- cats(x)[[1]]
+		if (!is.null(index)) {
+			if (!((index > 1) & (index <= ncol(g)))) {
+				error("as.numeric", "invalid index")
+			}
+		} else {
+			index <- setCats(x, 1)
+		}
+		from <- g[,1]
+		toc <- g[,index]
+		
+		addFact <- FALSE
+		if (!is.numeric(toc)) {
+			addFact <- TRUE
+			ton <- as.integer(as.factor(toc))
+		} else {
+			ton <- toc
+		}
+		m <- cbind(from, ton)
+		m <- m[!is.na(m[,1]), ,drop=FALSE]
+		x <- classify(x, m, names=names(g)[index], ...)
+		if (addFact) {
+			fact <- unique(data.frame(ton, toc))
+			names(fact) <- c("ID", names(g)[index])
+			fact <- fact[order(fact[,1]), ]
+			setCats(x, 1, fact, 2)
+		}
+		x
+	}
+)
+	
+	
 setMethod("catalyze", "SpatRaster", 
 	function(x, filename="", ...) {
 		g <- cats(x)
