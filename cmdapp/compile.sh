b1c9536ca468cc99e996c0fc237c8f304c6f455d
rhijmans <r.hijmans@gmail.com>
2022-01-15 12:11:28 -0800

    x

--- cmdapp/compile.sh
+++ cmdapp/compile.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 g++ -o terra -std=c++11 -I/usr/include/gdal  -I../src/   ../src/crs.cpp  ../src/ram.cpp \
-    ../src/spatVector.cpp ../src/spatRaster.cpp ../src/spatBase.cpp ../src/string_utils.cpp  ../src/gdal_multidimensional.cpp \
-    ../src/gdalio.cpp ../src/memory.cpp  ../src/math_utils.cpp  ../src/focal.cpp  ../src/arith.cpp ../src/distance.cpp \
-    ../src/read.cpp ../src/read_gdal.cpp  ../src/read_ogr.cpp ../src/file_utils.cpp  ../src/distRaster.cpp  ../src/geos_methods.cpp  ../src/gdal_algs.cpp \
-   ../src/raster_methods.cpp ../src/raster_stats.cpp ../src/rasterize.cpp ../src/spatSources.cpp  ../src/spatTime.cpp \
-   ../src/spatDataframe.cpp ../src/vecmath.cpp ../src/vector_methods.cpp ../src/write.cpp ../src/write_gdal.cpp  ../src/write_ogr.cpp main.cpp show.cpp  \
-   ../src/extract.cpp  -lgeos_c  -lgdal -lproj -ltiff -lgeotiff  -I/usr/include/gdal  -Dstandalone
+    ../src/spatVector.cpp ../src/spatRaster.cpp ../src/spatBase.cpp ../src/string_utils.cpp \
+	../src/gdal_multidimensional.cpp ../src/gdalio.cpp ../src/memory.cpp  ../src/math_utils.cpp \
+	../src/focal.cpp  ../src/arith.cpp ../src/distance.cpp ../src/read.cpp ../src/read_gdal.cpp \
+	../src/read_ogr.cpp ../src/file_utils.cpp  ../src/distRaster.cpp  ../src/geos_methods.cpp \
+	../src/gdal_algs.cpp ../src/raster_methods.cpp ../src/raster_stats.cpp ../src/rasterize.cpp \
+	../src/spatSources.cpp  ../src/spatTime.cpp ../src/spatDataframe.cpp ../src/vecmath.cpp \
+	../src/vector_methods.cpp ../src/write.cpp ../src/write_gdal.cpp  ../src/write_ogr.cpp 
+	../src/extract.cpp \
+	main.cpp show.cpp -lgeos_c  -lgdal -lproj -ltiff -lgeotiff  -I/usr/include/gdal  -Dstandalone
 
--- cmdapp/main.cpp
+++ cmdapp/main.cpp
@@ -6,32 +6,29 @@
 
 int main(int argc, char *argv[]) {
 
-    	//SpatRaster test;
-    	//std::cout <<  test.nrow() << std::endl;
-    	//show(test);
 	std::vector<std::string> arguments = std::vector<std::string>(argv, argv + argc);
 
  	arguments[0] = arguments[0].substr(arguments[0].find_last_of("/\\") + 1);
  	if (arguments.size() < 2) {
 		std::string msg = "usage: " +  arguments[0] + " method input output parameters";
 		std::cout << msg << std::endl;
-                return 1;
-        }
+        return 1;
+	}
 	GDALAllRegister();
 	SpatRaster out;
- 	std::string method = arguments[1];
+	std::string method = arguments[1];
 	//SpatRaster input(arguments[2], {-1}, {""});
-     	//show(input);
+    //show(input);
 
-        if (method == "show") out = SpatRaster(arguments[2], {-1}, {""}, {""});
-        if (method == "aggregate") out = aggregate(arguments);
+    if (method == "show") out = SpatRaster(arguments[2], {-1}, {""}, {""});
+    if (method == "aggregate") out = aggregate(arguments);
  
-        if (out.hasError()) {
-           	std::cout <<  out.msg.error << std::endl;
-           	return 1;
-        } else {
+    if (out.hasError()) {
+       	std::cout <<  out.msg.error << std::endl;
+       	return 1;
+    } else {
 		show(out);
-        }
+    }
 	return 0;
 }
 
