ccfdf3386808f6cf5dafbafdb91ba5a3eb6e0a62
Roger Bivand <roger.bivand@gmail.com>
2022-03-19 10:30:32 +0100

    fix wkt grd test for GDAL <= 3.4.2

--- inst/tinytest/test_wkt_grd.R
+++ inst/tinytest/test_wkt_grd.R
@@ -4,5 +4,10 @@ terra::crs(tst) <- "EPSG:28992"
 tf <- tempfile(fileext=".grd")
 terra::writeRaster(tst, tf)
 tst1 <- terra::rast(tf)
-expect_identical(terra::crs(tst, describe=TRUE)[1:3], terra::crs(tst1, describe=TRUE)[1:3])
-
+if (gdal() > "3.4.2") {
+    expect_identical(terra::crs(tst, describe=TRUE)[1:3],
+        terra::crs(tst1, describe=TRUE)[1:3])
+} else {
+    expect_false(isTRUE(all.equal(terra::crs(tst, describe=TRUE)[1:3],
+        terra::crs(tst1, describe=TRUE)[1:3])))
+}
