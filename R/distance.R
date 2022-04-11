0007a3f41ec537b05ae6e37d933454fc582a2aef
rhijmans <r.hijmans@gmail.com>
2022-04-10 19:20:46 -0700

    x

--- R/distance.R
+++ R/distance.R
@@ -31,18 +31,9 @@ setMethod("distance", signature(x="SpatRaster", y="missing"),
 )
 
 setMethod("costDistance", signature(x="SpatRaster"), 
-	function(x, target, scale=1000, filename="", ...) {
+	function(x, scale=1000, filename="", ...) {
 		opt <- spatOptions(filename, ...)
-		if (is.na(target)) {
-			error("costDistance", "target cannot be NA")		
-		}
-		if (!is.numeric(target)) {
-			error("costDistance", "target must be numeric")		
-		}		
-		if (length(target) > 1) {
-			error("costDistance", "target must be a single value")
-		}
-		x@ptr <- x@ptr$costDistance(target, scale[1], opt)
+		x@ptr <- x@ptr$costDistance(scale[1], opt)
 		messages(x, "cost distance")
 	}
 )
