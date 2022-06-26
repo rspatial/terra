0fbc02e2dcd9fc3d67e351207d922c958872d9e1
rhijmans <r.hijmans@gmail.com>
2022-06-26 12:51:23 -0700

    u

--- R/coerce.R
+++ R/coerce.R
@@ -514,7 +514,7 @@ setAs("im", "SpatRaster",
 setAs("SpatVector", "Spatial", 
 	function(from) {
 		if (!("geom,data.frame-method" %in% methods("geom"))) {
-			error("coerce", "run 'library(raster)' to coerce a SpatVector to a Spatial object" 
+			error("coerce", "first run 'library(raster)' to coerce a SpatVector to a Spatial object" )
 		}
 		g <- geom(from, df=TRUE)
 		geom(g, values(from), geomtype(from), as.character(crs(from)))
