8c1122d05fd3c8e3bd06153c1498fb9038c6701d
rhijmans <r.hijmans@gmail.com>
2022-09-26 19:19:30 -0700

    n

--- inst/tinytest/test_cats.R
+++ inst/tinytest/test_cats.R
@@ -53,7 +53,7 @@ expect_equal(levels(r2), list(lv, lv))
 
 # verify samples for layer a and layer b have categories
 expect_equal(r2[c(15,3)], data.frame(cover = factor(c("forest", "urban"), levels = lv[,2]),
-                                           cover.1 = factor(c("forest", "urban"), levels = lv[,2]), stringsAsFactors=FALSE))
+                                           cover = factor(c("forest", "urban"), levels = lv[,2]), stringsAsFactors=FALSE, check.names = FALSE))
 
 # make sure no errors when show()ing factors
 expect_silent(show(r2))
