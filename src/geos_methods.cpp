fbfef0d0aef83f1867500a925309757fa45cf7ba
rhijmans <r.hijmans@gmail.com>
2023-05-04 11:37:56 -0700

    ?

--- src/geos_methods.cpp
+++ src/geos_methods.cpp
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with spat. If not, see <http://www.gnu.org/licenses/>.
 
+
 #include <numeric>
 #include "geos_spat.h"
 #include "distance.h"
