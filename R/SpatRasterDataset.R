7206e8575879330ef439afe614be13cee6322971
rhijmans <r.hijmans@gmail.com>
2023-07-03 16:39:34 -0700

    x

--- R/SpatRasterDataset.R
+++ R/SpatRasterDataset.R
@@ -71,6 +71,14 @@ setMethod("sds", signature(x="list"),
 		for (i in seq_along(x)) {
 			if (inherits(x[[i]], "SpatRaster")) {
 				r@ptr$add(x[[i]]@ptr, nms[i], "", "", FALSE)
+			} else if (inherits(x[[i]], "SpatRasterDataset")) {
+				y <- as.list(x[[i]])
+				ynms <- names(x[[i]])
+				s <- sapply(y, \(j) ptr$add(j@ptr, ynms[j], "", "", FALSE))
+			} else {
+				name <- names(x[[i]])
+				cls <- paste(class(x[[i]]), collapse=", ")
+				error("sds", "list element", name, "is a: ", cls)
 			}
 		}
 		messages(r, "sds")
@@ -290,10 +298,14 @@ setMethod("sprc", signature(x="list"),
 			for (i in 1:n) {
 				if (inherits(x[[i]], "SpatRaster")) {
 					ptr$add(x[[i]]@ptr, "")
+				} else if (inherits(x[[i]], "SpatRasterCollection") | 
+							inherits(x[[i]], "SpatRasterDataset")) {
+					y <- as.list(x[[i]])
+					s <- sapply(y, \(j) ptr$add(j@ptr, ""))
 				} else {
 					name <- names(x[[i]])
-					cls <- class(x[[i]])
-					error("sprc", "list elements should be 'SpatRaster'\n", name, "is of class: ", cls)
+					cls <- paste(class(x[[i]]), collapse=", ")
+					error("sprc", "list element", name, "is a: ", cls)
 				}
 			}
 		}
@@ -370,3 +382,41 @@ setMethod("add<-", signature("SpatRasterDataset", "SpatRaster"),
 		messages(x, "add")
 	}
 )
+
+
+setMethod("c", signature(x="SpatRasterCollection"),
+	function(x, ..., warn=TRUE) {
+		y <- list(...)
+		sprc(c(as.list(x), ...))
+	}
+)
+
+
+setMethod("c", signature(x="SpatRasterCollection"),
+	function(x, ...) {
+
+		x@ptr <- x@ptr$deepcopy()
+
+		dots <- list(...)
+		nms <- names(dots)
+		for (i in seq_along(dots)) {
+			if (inherits(dots[[i]], "SpatRasterCollection") | inherits(dots[[i]], "SpatRasterDataset")) {
+				sdsnms <- names(dots[[i]])
+				for (j in 1:(length(dots[[i]]))) {
+					x@ptr$add(dots[[i]][[j]]@ptr, sdsnms[j])
+				}
+			} else if (inherits(dots[[i]], "SpatRaster")) {
+				if (is.null(nms)) { 
+					name <- paste0("d", i)
+				} else { 
+					name <- nms[i] 
+				}
+				x@ptr$add(dots[[i]]@ptr, name)
+			} else {
+				error("c", "arguments must be SpatRaster, SpatRasterCollection, or SpatRasterDataset")
+			}
+		}
+		messages(x, "c")
+	}
+)
+
