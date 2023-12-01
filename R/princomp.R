ab684a5cd5dada8bd5e95e43193cdd7e874d942c
rhijmans <r.hijmans@gmail.com>
2023-11-30 18:06:49 -0800

    na masking

--- R/princomp.R
+++ R/princomp.R
@@ -1,18 +1,19 @@
 
 
 setMethod("princomp", signature(x="SpatRaster"),
-	function(x, cor=FALSE, nobs=FALSE) {
+	function(x, cor=FALSE) {
 		stopifnot(nlyr(x) > 1)
 		xcov <- layerCor(x, fun="cov", na.rm=TRUE)
 		model <- princomp(covmat = xcov$covariance)
 		model$center <- diag(xcov$mean)
 		if (cor) {
 		## Calculate scale as population sd like in in princomp
-			model$n.obs  <- ncell(x) - global(anyNA(x), sum)$sum
+			aNA <- anyNA(x)
+			x <- mask(x, aNA, maskvalue=TRUE)
+			model$n.obs <- ncell(x) - global(aNA, sum)$sum
+			n.obs  <- ncell(x) - global(anyNA(x), sum)$sum
 			S <- diag(xcov$covariance)
 			model$scale <- sqrt(S * (model$n.obs-1) / model$n.obs)
-		} else if (nobs) {
-			model$n.obs  <- ncell(x) - global(anyNA(x), sum)$sum
 		}
 		model
 	}
