51b2b76ea9e542a0fc4f772a43e2ee8d9e3b883f
rhijmans <r.hijmans@gmail.com>
2023-12-01 12:12:48 -0800

    fix_sign

--- R/princomp.R
+++ R/princomp.R
@@ -1,17 +1,18 @@
 
 
 setMethod("princomp", signature(x="SpatRaster"),
-	function(x, cor=FALSE) {
+	function(x, cor=FALSE, fix_sign=TRUE) {
 		stopifnot(nlyr(x) > 1)
 		xcov <- layerCor(x, fun="cov", na.rm=TRUE)
-		model <- princomp(covmat = xcov$covariance, cor=cor)
+		model <- princomp(covmat = xcov$covariance, cor=cor, fix_sign=fix_sign)
 		model$center <- diag(xcov$mean)
+		n <- diag(xcov$n)
 		if (cor) {
 		## Calculate scale as population sd like in in princomp
 			S <- diag(xcov$covariance)
-			n <- diag(xcov$n)
 			model$scale <- sqrt(S * (n-1) / n)
 		}
+		model$n <- n
 		model
 	}
 )
