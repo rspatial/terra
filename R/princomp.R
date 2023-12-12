632c8d50ac5506806f0dbdc8a2891af170e743f1
Emanuele Cordano <emanuele.cordano@gmail.com>
2023-12-12 16:44:30 +0100

    updates - remove large file .RData

---
+++ R/princomp.R
@@ -0,0 +1,29 @@
+
+
+setMethod("princomp", signature(x="SpatRaster"),
+	function(x, cor=FALSE, fix_sign=TRUE, use="pairwise.complete.obs", maxcell=Inf) {
+
+		if (!hasValues(x)) {
+			error("princomp", "x has no values")
+		}
+		if (nlyr(x) < 2) {
+			error("princomp", "The number of layers of x must be > 1")
+		}
+		xcov <- layerCor(x, fun="cov", use=use, asSample=FALSE, maxcell=maxcell)
+		if (any(is.na(xcov[["covariance"]]))) {
+			error("princomp", "the covariance matrix has missing values")		
+		}
+
+		model <- princomp(covmat=xcov$covariance, cor=cor, fix_sign=fix_sign)
+		model$center <- diag(xcov$mean)
+		n <- diag(xcov$n)
+		if (cor) {
+		## scale as population sd
+			S <- diag(xcov$covariance)
+			model$scale <- sqrt(S)
+		}
+		model$n.obs <- stats::as.dist(xcov$n)
+		model
+	}
+)
+
---
+++ src/bufclipper.cpp
@@ -0,0 +1,95 @@
+
+#include "Rcpp.h"
+#include "spatVector.h"
+#include "recycle.h"
+#include "string_utils.h"
+#include "clipper.h"
+#include "clipper.core.h"
+//#include "../../Utils/clipper.svg.utils.h"
+//#include "../../Utils/CommonUtils.h"
+
+
+SpatVector SpatVector::buffer4(std::vector<double> d, std::string jointype, double miter_limit, int precision, double arc_tolerance) {
+//double miter_limit = 2.0, int precision = 2, double arc_tolerance = 0.0) {
+
+	SpatVector out;
+	size_t n = size();
+	recycle(d, n);
+	std::string vt = type();
+	SpatExtent e = getExtent();
+	if (precision < 0) {
+		double m = std::max(e.xmax - e.xmin, e.ymax - e.ymin);
+		m = std::round(sqrt(m));
+		precision = std::max(2, (int) (8 - m));
+	}
+	lowercase(jointype);
+	Clipper2Lib::JoinType jtype;
+	if (jointype == "square") {
+		jtype = Clipper2Lib::JoinType::Square;
+	} else if (jointype == "bevel") {
+		jtype = Clipper2Lib::JoinType::Bevel;
+	} else if (jointype == "miter") {
+		jtype = Clipper2Lib::JoinType::Miter;
+	} else { // round
+		jtype = Clipper2Lib::JoinType::Round;
+	}
+
+	if (vt == "points") {
+//		Clipper2Lib::EndType etype = Clipper2Lib::EndType::Round;
+		return out;
+	} else if (vt == "lines") {
+//		Clipper2Lib::EndType etype = Clipper2Lib::EndType::Joined;
+		return out;
+	} else { // polygons
+		Clipper2Lib::EndType etype = Clipper2Lib::EndType::Polygon;
+		Clipper2Lib::PointD pnt;
+		for (size_t i=0; i<n; i++) {
+			SpatGeom g = getGeom(i);
+			size_t np = g.size();
+			Clipper2Lib::PathsD input;
+			input.reserve(np);
+			for (size_t j=0; j < np; j++) {
+				SpatPart p = g.getPart(j);
+				Clipper2Lib::PathD part;
+				part.reserve(p.x.size());
+				for (size_t k=0; k<p.x.size(); k++) {
+					pnt.x = p.x[k];
+					pnt.y = p.y[k];
+					part.push_back(pnt);	
+				}
+				input.push_back(part);
+			}
+			Clipper2Lib::PathsD solution = InflatePaths(input, d[i], jtype, etype, miter_limit, precision, arc_tolerance);
+			
+			np = solution.size();
+			SpatGeom gg;
+			gg.gtype = polygons;
+			for (size_t j=0; j<np; j++) {
+				size_t nn = solution[j].size();
+				if (nn > 0) {
+					std::vector<double> X, Y;
+					X.reserve(nn+1);
+					Y.reserve(nn+1);
+					for (size_t k=0; k<nn; k++) {
+						X.push_back(solution[j][k].x);
+						Y.push_back(solution[j][k].y);
+					}
+					X.push_back(X[0]);
+					Y.push_back(Y[0]);
+					SpatPart pp(X, Y);
+					gg.addPart(pp);
+				}
+			}
+			if (gg.parts.size() > 0) {
+				out.addGeom(gg);
+			}
+		}
+	}
+   
+	out.srs = srs;
+	return out;
+	
+}
+
+
+	  
