0b3a60b5ee07fab1990c3a720ea7d658e928b025
rhijmans <r.hijmans@gmail.com>
2023-12-27 09:47:49 -0800

    fixes #1387

--- inst/tinytest/test_multivariate.R
+++ inst/tinytest/test_multivariate.R
@@ -6,4 +6,7 @@ r[1] <- NA
 km <- k_means(r, centers=5)
 km <- k_means(r, centers=5, maxcell=10)
 
-
+x <- rast(system.file("ex/logo.tif", package="terra"))   
+a <- layerCor(x, "cor")
+b <- layerCor(x, cor)
+expect_equivalent(a$correlation, b)
