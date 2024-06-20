984062fc09ad20b9842e5027d53f91d2222dd22e
rhijmans <r.hijmans@gmail.com>
2024-06-19 17:20:06 -0700

    for #1548

---
+++ inst/tinytest/test_rasterize.R
@@ -0,0 +1,8 @@
+
+v <- vect(system.file("ex/lux.shp", package = "terra"))
+r <- rast(v, ncols = 75, nrows = 100)
+z <- rasterize(v, r, cover = TRUE, by = "ID_2")
+v <- unlist(z[30*75+28])
+e <- c(0.01538462, NA, NA, NA, 0.9846154, NA, NA, NA, NA, NA, NA, NA)
+
+tinytest::expect_equivalent(v, e, tolerance=2e-07)
