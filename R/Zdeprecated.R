18b68badc460a98e34ed0bf194699e459084613e
rhijmans <r.hijmans@gmail.com>
2024-11-24 15:37:48 -0800

    m

--- R/Zdeprecated.R
+++ R/Zdeprecated.R
@@ -1,10 +1,20 @@
 
-#if (!isGeneric("area")) {setGeneric("area", function(x, ...) standardGeneric("area"))}
-#setMethod ("area" , "SpatRaster",
-#	function (x, ...) {
-#		error("area", "this method was removed. Use expanse or cellSize")
-#	}
-#)
+if (!isGeneric("area")) {setGeneric("area", function(x, ...) standardGeneric("area"))}
+setMethod ("area" , "SpatRaster",
+	function (x, ...) {
+		error("area", "this method was removed. Use expanse or cellSize")
+	}
+)
+
+if (!isGeneric("gridDistance")) {setGeneric("gridDistance", function(x, ...) standardGeneric("gridDistance"))}
+setMethod("gridDistance", signature(x="SpatRaster"),
+	function(x, ...) {
+		error("gridDistance", "'gridDistance' was renamed to 'gridDist'")
+		#. 'gridDistance' will be removed in a future version")
+		#gridDist(x, target=target, scale=scale, maxiter=maxiter, filename=filename, ...) 
+	}
+)
+
 
 
 #if (!isGeneric("setCats")) { setGeneric("setCats", function(x, ...) standardGeneric("setCats")) }
@@ -21,14 +31,6 @@
 #	function(x, target=0, scale=1, maxiter=50, filename="", ...) {
 #		warn("costDistance", "'costDistance' was renamed to 'costDist'. 'costDistance' will be removed in a #future version")
 #		costDist(x, target=target, scale=scale, maxiter=maxiter, filename=filename, ...)
-#	}
-#)
-
-#setMethod("gridDistance", signature(x="SpatRaster"),
-#	function(x, ...) {
-#		error("gridDistance", "'gridDistance' was renamed to 'gridDist'")
-#		#. 'gridDistance' will be removed in a future version")
-#		#gridDist(x, target=target, scale=scale, maxiter=maxiter, filename=filename, ...) 
 #	}
 #)
 
