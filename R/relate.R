0e46279baf7774a243e2f99fdda000c676f1a959
rhijmans <r.hijmans@gmail.com>
2025-01-24 20:05:06 -0800

    fixes #1728

--- R/relate.R
+++ R/relate.R
@@ -354,7 +354,7 @@ setMethod("nearest", signature(x="SpatVector"),
 		} else {
 			z@pntr <- x@pntr$near_between(y@pntr, pairs, method)
 		}
-		z <- messages(z, "nearest")
+		z <- terra:::messages(z, "nearest")
 		if (geomtype(z) == "points") { #lonlat points
 			if (lines) {
 				x <- z[,c(2,5), drop=TRUE]
@@ -378,7 +378,8 @@ setMethod("nearest", signature(x="SpatVector"),
 			from <- zz[seq(1, nrow(zz), 2), ]
 			to <- zz[seq(2, nrow(zz), 2), ]
 			values(to) <- data.frame(id=1:nrow(to))
-			to_int <- as.data.frame(intersect(to, y)[1])
+			to_int <- as.data.frame(intersect(to, y))
+			to_int <- to_int[!duplicated(to_int[,1]), ]
 			to_int <- as.data.frame(merge(to, to_int, by="id", all.x=TRUE))
 			if (any(is.na(to_int$to_id))) {
 				zz <- as.points(elongate(z, 1))
