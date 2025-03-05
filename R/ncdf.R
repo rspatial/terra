9c777b12b0581edd80436c10649e2fbdf42f713e
rhijmans <r.hijmans@gmail.com>
2025-03-05 11:27:30 -0800

    r

--- R/ncdf.R
+++ R/ncdf.R
@@ -102,7 +102,7 @@ get_time_vars <- function(y) {
 	} else {
 		xname <- "easting"
 		yname <- "northing"
-		linunit <- linearUnits(x)
+		linunit <- linearUnits(x[1])
 		if (isTRUE(linunit == 1)) {
 			xunit <- yunit <- "meter"
 		} else if (isTRUE(linunit == 1000)) {
