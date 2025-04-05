b2d1c06eae2280553fbd0f4100b86718c1f7d822
rhijmans <r.hijmans@gmail.com>
2025-04-05 15:07:17 -0700

    fixes #1802

--- inst/tinytest/test_tags.R
+++ inst/tinytest/test_tags.R
@@ -3,7 +3,9 @@ r <- rast(system.file("ex/elev.tif", package = "terra"))
 metags(r) <- "FOO=BAR"
 metags(r, layer = 1) <- "this=that"
 
-expect_equal(metags(r), data.frame(name=c("AREA_OR_POINT", "FOO"), value=c("Area", "BAR"), domain=""))
+metags(x) <- data.frame(name="FOO", value="BAR", domain="")
+
+expect_equal(metags(r), metags(x))
 expect_equal(metags(r, layer=1), data.frame(layer=1, name="this", value="that"))
 
 
