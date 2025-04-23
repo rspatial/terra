99adf30816a720a085e38d3f966f49d91b90c115
rhijmans <r.hijmans@gmail.com>
2025-04-23 04:19:11 -0700

    z

--- R/zzz.R
+++ R/zzz.R
@@ -39,25 +39,22 @@
 	}
 }
 
-loadModule("spat", TRUE)
+#loadModule("spat", TRUE)
 
 .onLoad <- function(libname, pkgname) {
-	.gdinit()
-	#registerMethods(list(c(package, genname, class)))
+#	.gdinit()
 }
 
 
 .onAttach <- function(libname, pkgname) {
 	packageStartupMessage("terra ", utils::packageVersion("terra"))
-	.create_options()
+#	.create_options()
 
-	if (length(grep(.geos_version(FALSE, TRUE), .geos_version(TRUE))) != 1) {
-		packageStartupMessage("WARNING: different compile-time and run-time versions of GEOS")
-		packageStartupMessage("Compiled with:", .geos_version(FALSE, TRUE))
-		packageStartupMessage(" Running with:", .geos_version(TRUE, TRUE))
-		packageStartupMessage("\nYou should reinstall package 'terra'\n")
-	}
-#	terraOptions(todisk=TRUE, steps=2)
-#	terraOptions(memfrac=0)
+#	if (length(grep(.geos_version(FALSE, TRUE), .geos_version(TRUE))) != 1) {
+#		packageStartupMessage("WARNING: different compile-time and run-time versions of GEOS")
+#		packageStartupMessage("Compiled with:", .geos_version(FALSE, TRUE))
+#		packageStartupMessage(" Running with:", .geos_version(TRUE, TRUE))
+#		packageStartupMessage("\nYou should reinstall package 'terra'\n")
+#	}
 }
 
