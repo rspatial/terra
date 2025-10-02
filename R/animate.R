9a96541729457ce3f21c54b7f4ba57630850b7db
rhijmans <r.hijmans@gmail.com>
2025-10-02 10:05:27 -0700

    for #1909

--- R/animate.R
+++ R/animate.R
@@ -1,7 +1,7 @@
 # comment
 
 setMethod("animate", signature(x="SpatRaster"),
-function(x, pause=0.25, main, range, maxcell=50000, n=1, ...) {
+function(x, pause=0.25, main, range=NULL, maxcell=50000, n=1, ...) {
 	if (missing(main)) {
 		main <- names(x)
 	}
@@ -9,12 +9,10 @@ function(x, pause=0.25, main, range, maxcell=50000, n=1, ...) {
 #	x <- spatSample(x, size=maxcell, method="regular", as.raster=TRUE, warn=FALSE)
 	x <- sampleRaster(x, maxcell, method="regular", replace=FALSE, ext=NULL, warn=FALSE, overview=TRUE)
 
-    if (missing(range)) {
-      mnmx <- minmax(x)
-      range <- c(min(mnmx[1,]), max(mnmx[2,]))
-    }
-    if (length(range) == 1 && is.na(range)) {
-      range <- minmax(x)
+    if (is.null(range)) {
+      range <- range(minmax(x, compute=TRUE))
+    } else if (any(is.na(range))) {
+      range <- NULL
     }
 
 	nl <- nlyr(x)
@@ -22,10 +20,7 @@ function(x, pause=0.25, main, range, maxcell=50000, n=1, ...) {
 	i <- 1
 	reps <- 0
     while (reps < n) {
-        if (is.matrix(range))
-          plot(x[[i]], main = main[i], range=range[, i], maxcell=Inf, ...)
-        else 
-          plot(x[[i]], main = main[i], range=range, maxcell=Inf, ...)
+        plot(x[[i]], main = main[i], range=range, maxcell=Inf, ...)
         grDevices::dev.flush()
 	    grDevices::dev.hold()
         Sys.sleep(pause)
