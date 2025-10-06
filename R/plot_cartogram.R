cfc21dbb3104a59973f77f33eedd68b2e5fa4e49
rhijmans <r.hijmans@gmail.com>
2025-10-06 14:26:56 -0700

    fixes #1912

--- R/plot_cartogram.R
+++ R/plot_cartogram.R
@@ -1,16 +1,22 @@
 
 setMethod("cartogram", signature(x="SpatVector"),
-	function(x, var, type)  {
+	function(x, var, type="nc")  {
 		if (geomtype(x) != "polygons") {
 			error("cartogram", "x must be polygons")
 		}
 		type <- match.arg(tolower(type), "nc")
 		stopifnot(var %in% names(x))
 		v <- as.numeric(as.vector(x[[var, drop=TRUE]]))
-		if (!any(!is.na(v))) stop(paste("no numeric values in", var))
-		if (any(v <= 0)) stop(paste("non-positive values in", var))
-		x <- x[!is.na(v)]
-		v <- v[!is.na(v)]
+		i <- !is.na(v)
+		x <- x[i]
+		v <- v[i]
+
+		i <- v > 0
+		x <- x[i]
+		v <- v[i]
+		
+		if (nrow(x) == 0) return(vect("POLYGON EMPTY"))
+
 		f <- v / max(v)
 		cxy <- crds(centroids(x, inside=TRUE))
 		r <- lapply(1:length(v), function(i) {
