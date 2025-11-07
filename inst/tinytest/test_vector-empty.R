316ceabfc063350999f8c1546d3cecf5efa27f20
Andrew Gene Brown <brown.andrewg@gmail.com>
2025-11-06 17:24:45 -0800

    test: schema mismatch null intersection

--- inst/tinytest/test_vector-empty.R
+++ inst/tinytest/test_vector-empty.R
@@ -28,3 +28,29 @@ expect_equal(nrow(vect(outfile, layer="empty")), 0)
 expect_equal(names(vect(outfile, layer="empty")), names(empty_v))
 
 unlink(outfile)
+
+outfile <- file.path(tempdir(), "schema_mismatch.gpkg")
+
+a <- vect("POLYGON ((0 0, 0 1, 1 1, 1 0, 0 0))")
+b <- vect("POLYGON ((10 10, 10 11, 11 11, 11 10, 10 10))")
+a$id <- 1L
+b$id <- "text"
+
+suppressWarnings({
+  empty_intersect <- intersect(a, b)
+})
+
+expect_equal(nrow(empty_intersect), 0)
+expect_equal(names(empty_intersect), c("id_1", "id_2"))
+
+writeVector(empty_intersect, outfile, overwrite=TRUE)
+
+read_result <- vect(outfile)
+expect_equal(nrow(read_result), 0)
+expect_equal(names(read_result), c("id_1", "id_2"))
+
+proxy_result <- vect(outfile, proxy=TRUE)
+expect_equal(nrow(proxy_result), 0)
+expect_equal(names(proxy_result), c("id_1", "id_2"))
+
+unlink(outfile)
