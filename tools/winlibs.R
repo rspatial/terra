11504d15410f1a73f8a512cee1a09432fc4a3016
rhijmans <r.hijmans@gmail.com>
2025-11-16 13:50:57 -0800

    tst

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -8,7 +8,7 @@ VERSION <- commandArgs(TRUE)
 # wrong path: if(!file.exists(sprintf("../windows/gdal3-%s/include/gdal/gdal.h", VERSION))){
 testfile <- sprintf("../windows/gdal3-%s/include/gdal-%s/gdal.h", VERSION, VERSION)
 if(!file.exists(testfile)){  
-  if(getRversion() < "3.3.0") setInternet2()
+#  if(getRversion() < "3.3.0") setInternet2()
   download.file(sprintf("https://github.com/rwinlib/gdal3/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
