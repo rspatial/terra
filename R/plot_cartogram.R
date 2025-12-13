e92afd389ce5b0a1e25798cd5b42c260c6e574d6
rhijmans <r.hijmans@gmail.com>
2025-12-13 12:00:41 -0800

    r

--- R/plot_cartogram.R
+++ R/plot_cartogram.R
@@ -1,6 +1,6 @@
 
 setMethod("cartogram", signature(x="SpatVector"),
-	function(x, var, type="nc", inside=FALSE, fudge=1)  {
+	function(x, var, type="nc", inside=FALSE, exp=1)  {
 		if (geomtype(x) != "polygons") {
 			error("cartogram", "x must be polygons")
 		}
@@ -19,18 +19,18 @@ setMethod("cartogram", signature(x="SpatVector"),
 		if (nrow(x) == 0) return(vect("POLYGON EMPTY"))
 
 		f <- v / max(v)
-		if (fudge <= 0) fudge <- 1
-		fudge <- sqrt(fudge)
+		if (exp <= 0) exp <- 1
+		exp <- sqrt(exp)
 		cntrds <- centroids(x, inside=inside)
 		if (type == "circles") {
 			w <- max(width(buffer(cntrds, width(x))))
-			ff <- fudge * (sqrt(f / pi) * w/4)
+			ff <- exp * (sqrt(f / pi) * w/4)
 			buffer(cntrds, ff)
 		} else {
 			cxy <- crds(cntrds)
 			s <- sqrt(expanse(x))
 			ff <- s[which.max(f)] / s
-			ff <- ff * sqrt(f) * fudge
+			ff <- ff * sqrt(f) * exp
 			r <- lapply(1:length(v), function(i) {
 				rescale(x[i,], ff[i], x0=cxy[i,1], y0=cxy[i,2])
 			})
