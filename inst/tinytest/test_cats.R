446abe4cd08b0296bc3e01f41913ac49c8d2e1c7
Andrew Gene Brown <brown.andrewg@gmail.com>
2026-01-12 11:52:44 -0800

    test: ensure names are set and SpatRaster is not a factor

--- inst/tinytest/test_cats.R
+++ inst/tinytest/test_cats.R
@@ -18,14 +18,14 @@ r <- droplevels(r)
 expect_equal(levels(r)[[1]][,2], c("forest", "water", "urban"))
 lv <- lv[-4,]
 
-# reading/writing categories 
+# reading/writing categories
 ftmp <- tempfile(fileext = ".tif")
 z <- writeRaster(r, ftmp, overwrite=TRUE)
 v <- cats(z)[[1]]
 expect_equal(v$value, 1:3)
 expect_equal(v$`land cover`, lv[,2])
 
-# reading/writing subset of categories 
+# reading/writing subset of categories
 levels(r) = cats(r)[[1]][2:3,]
 zz = writeRaster(r, ftmp, overwrite=TRUE)
 v <- cats(zz)[[1]]
@@ -44,7 +44,7 @@ levels(r2) <- lv
 expect_equal(levels(r2), list(lv, ""))
 
 # verify samples for layer a have categories
-expect_equal( r2[c(15,3)], data.frame(cover = factor(c("forest", "urban"), levels = lv[,2]), 
+expect_equal( r2[c(15,3)], data.frame(cover = factor(c("forest", "urban"), levels = lv[,2]),
                                            b = c(1, 3), stringsAsFactors=FALSE ))
 
 # set all layer categories
@@ -63,5 +63,14 @@ r <- rast(nrows=10, ncols=10, vals=sample(1:5, 100, replace=TRUE))
 levels(r) <- data.frame(ID=1:5, val1=101:105, val2=seq(0.1, 0.5, length.out=5))
 r_cat <- catalyze(r)
 expect_equal(nlyr(r_cat), 2)
+expect_equal(names(r_cat), c("val1", "val2"))
+expect_false(any(is.factor(r_cat)))
+expect_silent(show(r_cat))
 expect_equal(as.vector(values(r_cat[[1]])), as.vector(values(r)) + 100)
 expect_equal(as.vector(values(r_cat[[2]])), as.vector(values(r)) * 0.1)
+
+r_num <- as.numeric(r)
+expect_false(any(is.factor(r_num)))
+expect_equal(names(r_num), "val1")
+expect_equivalent(as.vector(values(r_num)), as.vector(values(r)))
+
