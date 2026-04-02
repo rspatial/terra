767f983e4c72120debb2089c18d9cd9f05e4663a
rhijmans <r.hijmans@gmail.com>
2026-04-01 19:47:30 -0700

    r

--- inst/tinytest/test_vrt.R
+++ inst/tinytest/test_vrt.R
@@ -4,8 +4,8 @@
 r <- terra::rast(ncols=100, nrows=100)
 values(r) <- 1:ncell(r)
 r2 <- r*2
-file_name_1 <- past0(tempfile(), ".tif")
-file_name_2 <- past0(tempfile(), ".tif")
+file_name_1 <- paste0(tempfile(), ".tif")
+file_name_2 <- paste0(tempfile(), ".tif")
 vrt_filename <- file.path(tempdir(),"test.vrt")
 terra::writeRaster(r, file_name_1)
 terra::writeRaster(r2, file_name_2)
