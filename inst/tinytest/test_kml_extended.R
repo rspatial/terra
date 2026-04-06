78eccf5b4335acfb267b27f18ea15398aa927d60
rhijmans <r.hijmans@gmail.com>
2026-04-05 17:16:12 -0700

    now

--- inst/tinytest/test_kml_extended.R
+++ inst/tinytest/test_kml_extended.R
@@ -3,41 +3,43 @@
 if (!requireNamespace("XML", quietly = TRUE)) {
 	exit_file("No XML package")
 }
-library(terra)
 
-kml <- c(
-	"<?xml version=\"1.0\" encoding=\"UTF-8\"?>",
-	"<kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document>",
-	"<Placemark><name>PN</name>",
-	"<ExtendedData><SchemaData>",
-	"<SimpleData name=\"id\">7</SimpleData>",
-	"<SimpleData name=\"camp_id\">c1</SimpleData>",
-	"</SchemaData></ExtendedData>",
-	"<Polygon><outerBoundaryIs><LinearRing>",
-	"<coordinates>0,0,0 1,0,0 1,1,0 0,1,0 0,0,0</coordinates>",
-	"</LinearRing></outerBoundaryIs></Polygon>",
-	"</Placemark></Document></kml>"
-)
-f <- tempfile(fileext = ".kml")
-writeLines(kml, f, useBytes = TRUE)
-on.exit(unlink(f), add = TRUE)
+{ # one block for on.exit 
 
-tab <- terra:::.read_kml_extended_table(f)
-expect_true(is.data.frame(tab))
-expect_equal(nrow(tab), 1L)
-expect_true("Name" %in% names(tab))
-expect_true(any(grepl("id", names(tab), ignore.case = TRUE)))
+	kml <- c(
+		"<?xml version=\"1.0\" encoding=\"UTF-8\"?>",
+		"<kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document>",
+		"<Placemark><name>PN</name>",
+		"<ExtendedData><SchemaData>",
+		"<SimpleData name=\"id\">7</SimpleData>",
+		"<SimpleData name=\"camp_id\">c1</SimpleData>",
+		"</SchemaData></ExtendedData>",
+		"<Polygon><outerBoundaryIs><LinearRing>",
+		"<coordinates>0,0,0 1,0,0 1,1,0 0,1,0 0,0,0</coordinates>",
+		"</LinearRing></outerBoundaryIs></Polygon>",
+		"</Placemark></Document></kml>"
+	)
+	f <- tempfile(fileext = ".kml")
+	on.exit(unlink(f), add = TRUE)
+	writeLines(kml, f, useBytes = TRUE)
 
-# KMZ wrap (needs working zip in utils::zip)
-writeLines(kml, f, useBytes = TRUE)
-zf <- tempfile(fileext = ".kmz")
-tr <- try(utils::zip(zf, f), silent = TRUE)
-if (!inherits(tr, "try-error") && file.exists(zf)) {
+	tab <- terra:::.read_kml_extended_table(f)
+	expect_true(is.data.frame(tab))
+	expect_equal(nrow(tab), 1L)
+	expect_true("Name" %in% names(tab))
+	expect_true(any(grepl("id", names(tab), ignore.case = TRUE)))
+
+	# KMZ wrap (needs working zip in utils::zip)
+	zf <- tempfile(fileext = ".kmz")
 	on.exit(unlink(zf), add = TRUE)
-	ex <- terra:::.kml_extract_from_kmz(zf)
-	expect_true(!is.null(ex))
-	expect_true(file.exists(ex$path))
-	tab2 <- terra:::.read_kml_extended_table(ex$path)
-	expect_equal(nrow(tab2), 1L)
-	unlink(ex$dir, recursive = TRUE)
+	writeLines(kml, f, useBytes = TRUE)
+	tr <- try(utils::zip(zf, f), silent = TRUE)
+	if (!inherits(tr, "try-error") && file.exists(zf)) {
+		ex <- terra:::.kml_extract_from_kmz(zf)
+		expect_true(!is.null(ex))
+		expect_true(file.exists(ex$path))
+		tab2 <- terra:::.read_kml_extended_table(ex$path)
+		expect_equal(nrow(tab2), 1L)
+		unlink(ex$dir, recursive = TRUE)
+	}
 }
